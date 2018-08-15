08867e7780b74f77278bd98e0975376ad49e7107
abin <277690572@qq.com>
2018-08-15 18:58:06 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display: flex
+    width: 100%
+    height:40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>I am header组件</div>
+</template>
+
+<script>
+  export default {
+    name: 'header'
+  }
+</script>
+
+<style>
+</style>
