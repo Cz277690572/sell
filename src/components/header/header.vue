5c38ad6fc7c4bd1e0a43287d426fc153b0ac4ada
abin <277690572@qq.com>
2018-08-21 20:54:57 +0800

    header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -18,6 +18,31 @@
         </div>
       </div>
     </div>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%"/>
+    </div>
+    <div v-show="detailShow" class="detail" transition="fade">
+      <div class="detail-wrapper">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <p class="content">{{seller.bulletin}}</p>
+          </div>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,6 +53,16 @@
       seller: {
         type: Object
       }
+    },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail: function() {
+        this.detailShow = true
+      }
     }
   }
 </script>
@@ -35,8 +70,10 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin";
   .header
+    position: relative
     color: #fff
-    background: #000
+    overflow: hidden
+    background: rgba(7,17,27,0.5)
     .content-wrapper
       padding: 24px 12px 18px 24px
       font-size: 0
@@ -51,26 +88,117 @@
         font-size: 14px
         .title
           margin: 2px 0 8px 0
-          //.brand
-            //display: inline-block
-            //vertical-align: top
-            //width: 30px
-            //height: 18px
-            //bg-image("brand")
-            //background-size: 30px 18px
-            //background-repeat: no-repeat
+          .brand
+            display: inline-block
+            vertical-align: top
+            width: 30px
+            height: 18px
+            bg-image("brand")
+            background-size: 30px 18px
+            background-repeat: no-repeat
           .name
-            /*margin-left: 6px*/
+            margin-left: 6px
             font-size: 16px
             line-height: 18px
             font-weight: bold
-      .description
-        margin-bottom:10px
-        line-height: 12px
-        font-size: 12px
-      .support
-        .text
+        .description
+          margin-bottom:10px
           line-height: 12px
           font-size: 12px
+        .support
+          .text
+            line-height: 12px
+            font-size: 12px
 
+    .bulletin-wrapper
+      height: 28px
+      line-height: 28px
+      padding: 0 22px 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin-left: 4px
+        margin-right: 4px
+        margin-bottom: 0
+        margin-top: 0
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
+    .background
+      position: absolute;
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      transition: all 0.5s
+      backdrop-filter: blur(10px)
+      background: rgba(7, 17, 27, 0.8)
+      &.fade-transition
+        opacity: 1
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter, &.fade-leave
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
+      .detail-wrapper
+        min-height: 100%
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+          .title
+            display: flex
+            width: 80%
+            margin: 60px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding: 0 12px
+              font-weight: 700
+              font-size: 14px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
