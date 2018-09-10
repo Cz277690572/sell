8583c317e2dac528e39b9d1cf32ae52b3a2d882e
abin <277690572@qq.com>
2018-09-10 20:40:06 +0800

    add payRes-css

--- src/components/orderdetail/orderdetail.vue
+++ src/components/orderdetail/orderdetail.vue
@@ -1,81 +1,117 @@
 <template>
   <transition name="move">
     <div v-show="showFlag" class="order-detail" ref="orders">
-      <div>
-        <div class="title">
-          <i class="icon-arrow_lift" @click="hidden()"></i>
-          订单详情
-        </div>
-        <div v-show="from=='order'" class="order-info">
-          <div class="order-summary">
-            <div class="order-time-no">
-              <div class="order-time">
-                <span>下单时间:</span>
-                <span class="text">{{order.create_at}}</span>
-              </div>
-              <div class="order-no">
-                <span>订单编号:</span>
-                <span class="text">{{order.order_no}}</span>
-              </div>
+      <div class="title">
+        <i class="icon-arrow_lift" @click="hidden()"></i>
+        订单详情
+      </div>
+      <div v-show="from=='order'" class="order-info">
+        <div class="order-summary">
+          <div class="order-time-no">
+            <div class="order-time">
+              <span>下单时间:</span>
+              <span class="text">{{order.create_at}}</span>
             </div>
-            <div class="order-status">
-              <span v-show="order.status=='未支付'" class="unpay">未支付</span>
-              <span v-show="order.status=='已支付'" class="payed">等待商家发货</span>
-              <span v-show="order.status=='已发货'" class="done">已发货</span>
-              <span v-show="order.status=='订单已完成' || order.status=='已收货'" class="complete">订单已完成</span>
-              <span v-show="order.status=='订单已失效'" class="shut">订单已失效</span>
+            <div class="order-no">
+              <span>订单编号:</span>
+              <span class="text">{{order.order_no}}</span>
             </div>
           </div>
-          <div class="order-contact-info">
-            <div class="user-info">
-              <div class="name">
-                <span>联系人:</span>
-                <span class="text">{{order.express_username}}</span>
-              </div>
-              <div class="phone">
-                <span>手机号:</span>
-                <span class="text">{{order.express_phone}}</span>
-              </div>
-            </div>
-            <div class="address">
-              <span>{{order.express_address}}</span>
-            </div>
+          <div class="order-status">
+            <span v-show="order.status=='未支付'" class="unpay">未支付</span>
+            <span v-show="order.status=='已支付'" class="payed">等待商家发货</span>
+            <span v-show="order.status=='已发货'" class="done">已发货</span>
+            <span v-show="order.status=='订单已完成' || order.status=='已收货'" class="complete">订单已完成</span>
+            <span v-show="order.status=='订单已失效'" class="shut">订单已失效</span>
           </div>
         </div>
-        <div v-show="from=='cart'" class="receipt-info">
-          <div class="contacts">
-            <input name="username" placeholder="联系人 (必填)" v-model="receipt.username" ref="username"/>
-          </div>
-          <div class="phone">
-            <input name="phone" placeholder="联系手机号 (必填)" v-model="receipt.phone" ref="phone"/>
+        <div class="order-contact-info">
+          <div class="user-info">
+            <div class="name">
+              <span>联系人:</span>
+              <span class="text">{{order.express_username}}</span>
+            </div>
+            <div class="phone">
+              <span>手机号:</span>
+              <span class="text">{{order.express_phone}}</span>
+            </div>
           </div>
           <div class="address">
-            <input name="address" placeholder="收货地址 (必填)" v-model="receipt.address" ref="address"/>
-          </div>
-          <div class="desc">
-            <input name="desc" placeholder="备注 (选填)" v-model="receipt.desc" ref="desc"/>
+            <span>{{order.express_address}}</span>
           </div>
         </div>
-        <div class="goods" ref="goods">
-          <div class="goods-wrapper">
-            <div class="item" v-for="(item, index) in goods" :key="index">
-              <div class="item-left">
-                <img height="64px" width="64px"
-                     :src="item.icon">
+      </div>
+      <div v-show="from=='cart'" class="receipt-info">
+        <div class="contacts">
+          <input name="username" placeholder="联系人 (必填)" v-model="receipt.username" ref="username"/>
+        </div>
+        <div class="phone">
+          <input name="phone" placeholder="联系手机号 (必填)" v-model="receipt.phone" ref="phone"/>
+        </div>
+        <div class="address">
+          <input name="address" placeholder="收货地址 (必填)" v-model="receipt.address" ref="address"/>
+        </div>
+        <div class="desc">
+          <input name="desc" placeholder="备注 (选填)" v-model="receipt.desc" ref="desc"/>
+        </div>
+      </div>
+      <div class="goods" ref="goods">
+        <div class="goods-wrapper">
+          <div class="item" v-for="(item, index) in goods" :key="index">
+            <div class="item-left">
+              <img height="64px" width="64px"
+                   :src="item.icon">
+            </div>
+            <div class="item-middle">
+              <div class="good-name">{{item.name}}</div>
+              <div class="good-price">￥{{item.price}}</div>
+            </div>
+            <div class="item-right">{{item.count}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="order-account" v-show="orderStatus=='未支付'">
+        <div class="total-count">付款合计: ￥{{totalPrice}}</div>
+        <div class="to-pay" @click="checkPay" :class="payClass">{{payDesc}}</div>
+      </div>
+      <transition name="paymove">
+        <div class="pay-wrapper" v-show="payShow">
+          <div class="pay-result">
+            <div class="success" v-show="payRes=='success'">
+              <div class="pay-icon">
+                <img width="64" height="64" src="./pay-success.png">
               </div>
-              <div class="item-middle">
-                <div class="good-name">{{item.name}}</div>
-                <div class="good-price">￥{{item.price}}</div>
+              <div class="title">
+                <span>支付成功！</span>
               </div>
-              <div class="item-right">{{item.count}}</div>
+              <div class="action">
+                <div class="go-back" @click="_goBack">
+                  <span>返回首页</span>
+                </div>
+                <div class="go-order" @click="_goOrder">
+                  <span>查看订单</span>
+                </div>
+              </div>
+            </div>
+            <div class="error" v-show="payRes=='error'">
+              <div class="pay-icon">
+                <img width="64" height="64" src="./pay-error.png">
+              </div>
+              <div class="title">
+                <span>支付失败！</span>
+              </div>
+              <div class="action">
+                <div class="go-back" @click="_goBack">
+                  <span>返回首页</span>
+                </div>
+                <div class="go-order" @click="_goOrder">
+                  <span>重新支付</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="order-account" v-show="orderStatus=='未支付'">
-          <div class="total-count">付款合计: ￥{{totalPrice}}</div>
-          <div class="to-pay" @click="checkPay" :class="payClass">{{payDesc}}</div>
-        </div>
-      </div>
+      </transition>
     </div>
   </transition>
 </template>
@@ -120,7 +156,9 @@
         payDesc: '',
         goods: [],
         order: {},
-        orderStatus: '未支付'
+        orderStatus: '未支付',
+        payShow: false,
+        payRes: ''
       }
     },
     components: {},
@@ -203,10 +241,31 @@
         }
         console.log('提交参数审核通过，提交订单;返回订单id,清空购物车,提交订单id，拉起微信支付，返回支付结果, 跳转至支付结果')
         // 求参数{"username":"阿猫","phone":"15220501265","address":"2栋622","desc":"不要加辣","goods":[{"goods_id":"2","count":"2"},{"goods_id":"1","count":"1"}]}
-        this._emptyCart()
+        this.payShow = true
+        this.payRes = 'success'
+      },
+      _goBack() {
+        if (this.from === 'cart') {
+          this.payShow = false
+          this.showFlag = false
+          this._emptyCart()
+        } else if (this.from === 'order') {
+          this.$router.push({path: '/goods'})
+        }
+      },
+      _goOrder() {
+        if (this.from === 'cart') {
+          this._emptyCart()
+          this.$router.push({path: '/orders'})
+        } else if (this.from === 'order') {
+          this.payShow = false
+          this.showFlag = false
+        }
       },
       _fromOrder() {
         console.log('提交订单ID，拉起微信支付, 返回支付结果, 跳转至支付结果')
+        this.payShow = true
+        this.payRes = 'success'
       },
       _payDesc() {
         if (this.sellerStatus === 0) {
@@ -376,4 +435,62 @@
         &.enough
           background: #00b43c
           color: #fff
+    .pay-wrapper
+      opacity: 1
+      position: fixed
+      z-index: 200
+      top: 0
+      left: 0
+      bottom: 0
+      width: 100%
+      height: 100%
+      backdrop-filter: blur(10px)
+      background: rgba(7, 17, 27, 0.8)
+      backdrop-filter: blur(10px)
+      &.paymove-enter-active, &.paymove-leave-active
+        transition: all 0.5s
+      &.paymove-enter, &.paymove-leave-to
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
+      .pay-result
+        margin: 180px 60px
+        padding: 8px 8px
+        border-radius: 8px
+        background: #fff
+        .success, .error
+          .pay-icon
+            padding: 48px 0 28px 0
+            text-align: center
+          .title
+            line-height: 48px
+            font-size: 28px
+            background: #fff
+            color: #57AB53
+          .action
+            display: flex
+            text-align: center
+            line-height: 28px
+            font-size: 14px
+            padding-bottom: 9px
+            .go-back
+              flex: 1
+              margin: 10px 10px
+              color: rgb(7, 17, 27)
+              border: 1px solid #57AB53
+              border-radius: 5px
+            .go-order
+              flex: 1
+              color: #fff
+              border-radius: 5px
+              background: #57AB53
+              margin: 10px 10px
+        .error
+          .title
+            color: #DDB477
+          .action
+            .go-back
+              border: 1px solid rgba(7, 17, 27, 0.8)
+            .go-order
+              border: 1px solid #DDB477
+              background: #DDB477
 </style>
